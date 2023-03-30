d351d2fdcc6e70f847361eb4104fcde5ff78c0dc
Juan <juan@gmail.com>
2023-03-30 10:16:11 +0700

    change

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
             $servername = "localhost";
             $username = "root";
             $password = "";
-            $database = "usa_tranquillero";
+            $database = "usa_tranquillero.sql";
 
             // Create connection
             $connection = new mysqli($servername, $username, $password, $database);
--- read.php
+++ read.php
@@ -1,6 +1,6 @@
 <?php
 $connection = mysqli_connect("localhost","root","");
-$db = mysqli_select_db($connection, 'usa_tranquillero');
+$db = mysqli_select_db($connection, 'usa_tranquillero.sql');
 
 
 if(isset($_POST['submit']))
