7267b28fae8f53796df24747842b43cc612d1fce
Juan <juan@gmail.com>
2023-03-30 11:10:57 +0700

    change

--- index.php
+++ index.php
@@ -8,21 +8,28 @@
    <title>My Page</title>
    <link rel="stylesheet" href="css/font-awesome.min.css">
    <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="//sqltools/connections/FirstMySQLConnection/tranquillero_jenamel/table/student">
+   <link rel="stylesheet" href="/create.php">
+   <link rel="stylesheet" href="mysql/localhost/read_db/jenamel">
+   <link rel="stylesheet" href="./read.php">
+   <link rel="stylesheet" href="./edit.php">
+   <link rel="stylesheet" href="./delete.php">
+
 </head>
 
 <body>
    <div class="container my-5">
       <h2>List of Students</h2>
-      <a class="btn btn-primary" href="create.php" role="button">New Client</a>
+      <a class="btn btn-primary" href=" /tranquillero_jenamel/create.php" role="button">New Client</a>
       <br>
       <table class="table">
          <thead>
             <tr>
-               <th scope="col">ID</th>
-               <th scope="col">Email</th>
-               <th scope="col">Name</th>
-               <th scope="col">Message</th>
-               <th scope="col">Action</th>
+               <th scope = "col">ID</th>
+               <th scope = "col">Email</th>
+               <th scope = "col">Name</th>
+               <th scope = "col">Message</th>
+               <th scope = "col">Action</th>
             </tr>
          </thead>
          <tbody>
@@ -47,6 +54,7 @@
             if (!$result) {
                die("Invalid query: " . $connection->error);
             }
+            
 
             // read data of each row
             foreach ($result->fetch_all() as $key => $value) : ?>
@@ -56,8 +64,11 @@
                   <td><?= $value['2'] ?></td>
                   <td><?= $value['3'] ?></td>
                   <td>
-                     <a class='btn btn-primary btn-sm' href=' ./read.php/edit.php?id=$row[id]'>Edit</a>
-                     <a class='btn btn-primary btn-sm' href='./read.php/delete.php?id=$row[id]'>Delete</a>
+                  <td>
+                        <form action="delete.php" method="post">
+                           <input type="hidden" name="id" value="<?php echo $rows['id']?>">
+                           <input type="submit" name="delete" class="btn btn-outline-danger btn-sm" value="DELETE">
+                           </form>
                   </td>
                </tr>
             <?php endforeach ?>
@@ -65,6 +76,7 @@
       </table>
    </div>
    <script src="js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
