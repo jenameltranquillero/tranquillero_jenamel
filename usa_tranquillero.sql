a2ba537bd27537ad6d67ea1a6bfb61fb6baa0709
Juan <juan@gmail.com>
2023-03-30 10:45:23 +0700

    change

--- usa_tranquillero.sql
+++ usa_tranquillero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2023 at 04:11 PM
+-- Generation Time: Mar 30, 2023 at 05:18 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -18,21 +18,35 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `read_db`
+-- Database: `usa_tranquillero.sql`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jenamel`
+-- Table structure for table `usa_tranquillero`
 --
 
-CREATE TABLE `jenamel` (
+CREATE TABLE `usa_tranquillero` (
   `id` int(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `message` varchar(100) NOT NULL
+  `email` varchar(191) NOT NULL,
+  `name` varchar(191) NOT NULL,
+  `message` varchar(191) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usa_tranquillero`
+--
+
+INSERT INTO `usa_tranquillero` (`id`, `email`, `name`, `message`) VALUES
+(0, '', '', 'hi'),
+(0, '', '', 'hi'),
+(0, '', '', 'hello'),
+(0, '', '', 'ghtr'),
+(0, '', '', 'jjj'),
+(0, '', '', 'hi'),
+(0, '', '', 'hi'),
+(0, '', '', 'hello');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
